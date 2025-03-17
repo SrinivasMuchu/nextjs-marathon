bba030f03f266db97ac31421274cf364dc54a281
Uday Khatry <udaykhatry@Udays-MacBook-Pro.local>
2025-03-17 21:12:43 +0530

    bug fix

--- src/app/tools/cad-renderer/page.js
+++ src/app/tools/cad-renderer/page.js
@@ -1,22 +1,8 @@
-
-export const metadata = {
-  title: "View & Explore CAD Files Online | FreeCAD-Powered 3D Viewer | Marathon OS",
-  description:
-    "Render and explore CAD files online with our FreeCAD-powered 3D viewer. No downloads neede: view parts, assemblies, and designs seamlessly in your browser. Perfect for automotive, battery manufacturing, and hardware industries.",
-    robots: "noindex, nofollow",
-    openGraph: {images: [
-      {
-        url: "https://marathon-web-assets.s3.ap-south-1.amazonaws.com/logo-1.png",
-        width: 1200,
-        height: 630,
-        type: "image/png",
-      },
-    ],} , metadataBase: new URL("https://marathon-os.com"),
-    alternates: {
-      canonical: "/tools/cad-renderer",
-    },
-};
-import PartDesignView from '@/Components/PDMViewer/PartDesignView'
+"use client";
+import dynamic from "next/dynamic";
+const PartDesignView = dynamic(() => import("@/Components/PDMViewer/PartDesignView"), {
+  ssr: false, // ✅ Prevents SSR issues by loading only in the browser
+});
 import React from 'react'
 
 function DesignView() {
