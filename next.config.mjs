fbf6130f7e575a2cb66869bbb0b2f917be32fc94
Uday Khatry <udaykhatry@Udays-MacBook-Pro.local>
2025-04-27 20:29:53 +0530

    redirect urls

--- next.config.mjs
+++ next.config.mjs
@@ -23,6 +23,19 @@ const nextConfig = {
       // domains: ['marathon-web-assets.s3.ap-south-1.amazonaws.com','d1d8a3050v4fu6.cloudfront.net'], 
     }, 
     reactStrictMode: false,
-    
+    async redirects() {
+      return [
+        {
+          source: '/industry/aerospace-and-defence/:slug*',
+          destination: '/industry/aerospace-and-aviation/:slug*',
+          permanent: true,
+        },
+        {
+          source: '/industry/video-games-and-vr/:slug*',
+          destination: '/industry/video-games-and-virtual-reality/:slug*',
+          permanent: true,
+        },
+      ];
+    },
   };
 export default nextConfig;
