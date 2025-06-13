747958f366717fd62829275f6a048df3ed39705b
Uday Khatry <udaykhatry@Udays-MacBook-Pro.local>
2025-06-13 08:52:29 +0530

    changes

--- src/Components/History/FileHistoryCards.js
+++ src/Components/History/FileHistoryCards.js
@@ -169,12 +169,12 @@ function FileHistoryCards({ cad_type, currentPage, setCurrentPage, totalPages, s
                         localStorage.setItem("last_viewed_cad_key", file._id);
                       }}
                     >
-                      <Image
-                        src={`https://d1d8a3050v4fu6.cloudfront.net/${file._id}/sprite_90_180.webp`}
-                        alt="file preview"
-                        width={300}
-                        height={250}
-                      />
+                     {file.status === 'COMPLETED' ? <Image
+                          src={`https://d1d8a3050v4fu6.cloudfront.net/${file._id}/sprite_90_180.webp`}
+                          alt="file preview"
+                          width={300}
+                          height={160}
+                        /> : <div style={{ width: '100%', height: '160px', background: '#e6e4f0', display: 'flex', justifyContent: 'center', alignItems: 'center' }} />}
                       <div style={{ width: '100%', height: '2px', background: '#e6e4f0', marginBottom: '5px' }}></div>
 
                       <div className={styles.historyFileDetails}>
