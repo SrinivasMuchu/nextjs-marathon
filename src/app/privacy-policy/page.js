27ac5a89c20a3bd155c985ae6688cc190d0bfca9
unknown <srinivasmuchu14@gmail.com>
2025-07-22 12:44:16 +0530

    adout us

--- src/app/privacy-policy/page.js
+++ src/app/privacy-policy/page.js
@@ -2,7 +2,7 @@
 export const metadata = {
     title: "Privacy Policy | Marathon OS",
     description:
-      "Privacy policy",
+      "Learn how Marathon OS collects, uses, and protects your personal data. We value your privacy and are committed to safeguarding your information.",
       openGraph: {images: [
         {
           url: "https://marathon-web-assets.s3.ap-south-1.amazonaws.com/logo-1.png",
