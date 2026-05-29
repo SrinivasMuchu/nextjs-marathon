492eecfa44267ee6a7bf59a44c1ba448936d2123
unknown <srinivasmuchu14@gmail.com>
2026-05-29 11:44:46 +0530

    changes in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,6 +28,7 @@ const roboto = Roboto({
   adjustFontFallback: true,
 });
 const GA_TRACKING_ID = "G-6P47TN4FMC";
+const LINKEDIN_PARTNER_ID = "9449508";
 const BASE_URL = "https://marathon-os.com";
 
 const organizationJsonLd = {
@@ -197,6 +198,49 @@ export default function RootLayout({ children }) {
     `,
           }}
         />
+
+        <Script
+          id="linkedin-insight-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+      _linkedin_partner_id = "${LINKEDIN_PARTNER_ID}";
+      window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+      window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+    `,
+          }}
+        />
+
+        <Script
+          id="linkedin-insight"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+      (function(l) {
+        if (!l) {
+          window.lintrk = function(a, b) { window.lintrk.q.push([a, b]); };
+          window.lintrk.q = [];
+        }
+        var s = document.getElementsByTagName("script")[0];
+        var b = document.createElement("script");
+        b.type = "text/javascript";
+        b.async = true;
+        b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+        s.parentNode.insertBefore(b, s);
+      })(window.lintrk);
+    `,
+          }}
+        />
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            alt=""
+            src={`https://px.ads.linkedin.com/collect/?pid=${LINKEDIN_PARTNER_ID}&fmt=gif`}
+          />
+        </noscript>
 
         {/* <Script
   id="microsoft-clarity"
